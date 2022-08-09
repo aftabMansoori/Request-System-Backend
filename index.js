a24c1bea170f6e377029b14049ec5e5d9e26ebc4
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2022-08-10 00:48:05 +0530

    initialised mongoose

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const { connect } = require("./models/db");
+connect();
+
+const path = require("path");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use(helmet());
 app.use(morgan("combined"));
 
